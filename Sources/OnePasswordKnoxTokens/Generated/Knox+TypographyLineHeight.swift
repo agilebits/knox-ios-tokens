6455a825f970d605eb388e7e6604dd2f8592bde7
Katie Davis <katie.davis@agilebits.com>
2022-12-13 13:07:08 -0500

    Publishing version 3.0.2 of OnePasswordKnoxTokens.

--- Sources/OnePasswordKnoxTokens/Generated/Knox+TypographyLineHeight.swift
+++ Sources/OnePasswordKnoxTokens/Generated/Knox+TypographyLineHeight.swift
@@ -5,14 +5,14 @@ extension Knox {
 }
 
 extension Knox.TypographyLineHeight {
-	public static let default = 1.0
+	public static let `default` = 1.0
 	public static let headingLarge = 0.96
 	public static let headingXl = 0.92
 }
 
 extension Knox.TypographyLineHeight {
 	public enum Name: String {
-		case default = "default"
+		case `default` = "default"
 		case headingLarge = "headingLarge"
 		case headingXl = "headingXl"
 	}
@@ -20,7 +20,7 @@ extension Knox.TypographyLineHeight {
 
 extension Knox.TypographyLineHeight {
 	public static let allTokens: [Knox.TypographyLineHeight.Name : Knox.TokenMetadata] = [
-		Knox.TypographyLineHeight.Name.default : .init(name: "default", path: ["line-height", "default"], sortIndex: 0, value: .double(Knox.TypographyLineHeight.default)),
+		Knox.TypographyLineHeight.Name.`default` : .init(name: "default", path: ["line-height", "default"], sortIndex: 0, value: .double(Knox.TypographyLineHeight.`default`)),
 		Knox.TypographyLineHeight.Name.headingLarge : .init(name: "headingLarge", path: ["line-height", "heading", "large"], sortIndex: 1, value: .double(Knox.TypographyLineHeight.headingLarge)),
 		Knox.TypographyLineHeight.Name.headingXl : .init(name: "headingXl", path: ["line-height", "heading", "xl"], sortIndex: 2, value: .double(Knox.TypographyLineHeight.headingXl))
 	]
